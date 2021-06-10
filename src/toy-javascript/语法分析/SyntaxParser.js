a7df944793002373bd2c48c7c0f08af806d1bdb2
kokudolly <1226838252@qq.com>
2021-06-11 00:46:13 +0800

    feat: 递归求closure

--- src/toy-javascript/语法分析/SyntaxParser.js
+++ src/toy-javascript/语法分析/SyntaxParser.js
@@ -1,5 +1,6 @@
 // ll lr
 // 终结符 非终结符
+// token   terminal symbol
 let syntax = {
   Program: [['StatementList', 'EOF']],
   StatementList: [['Statement'], ['StatementList', 'Statement']],
@@ -9,7 +10,8 @@ let syntax = {
     ['VariableDeclaration'],
     ['FunctionDeclaration'],
   ],
-  ExpressionStatement: [['AdditiveExpression']],
+  ExpressionStatement: [['Expression', ';']],
+  Expression: [['AdditiveExpression']],
   AdditiveExpression: [
     ['MultiplicativeExpression'],
     ['AdditiveExpression', '+', 'MultiplicativeExpression'],
@@ -31,25 +33,49 @@ let syntax = {
 
 // 第一个接受的终结符
 // 展开非终结符的过程 求 closure 广度优先搜索
-let end = {
-  isEnd: true,
-}
-let start = {
-  Program: end,
-}
+let hash = {}
 function closure(state) {
+  hash[JSON.stringify(state)] = state
   let queue = []
   for (let symbol in state) {
     queue.push(symbol)
   }
+  // 展开一层closure
   while (queue.length) {
     let symbol = queue.shift()
-    if (syntax[symbol])
+    console.log(symbol)
+    if (syntax[symbol]) {
       for (let rule of syntax[symbol]) {
-        queue.push(rule[0])
-        console.log(rule[0])
+        if (!state[rule[0]]) {
+          queue.push(rule[0])
+          // state[rule[0]] = true // 删掉
+        }
+        let current = state
+        for (let part of rule) {
+          if (!current[part]) {
+            current[part] = {}
+          }
+          current = current[part] // 前进一步
+        }
+        current.$isRuleEnd = true
       }
+    }
   }
+
+  for (let symbol in state) {
+    if (!hash[JSON.stringify(state[symbol])]) {
+      closure(state[symbol])
+    } else {
+      state[symbol] = hash[JSON.stringify(state[symbol])]
+    }
+  }
+}
+
+let end = {
+  $isEnd: true,
+}
+let start = {
+  Program: end,
 }
 
 closure(start)
