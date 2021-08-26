9f276dd9d199acfe4912c27aef0abf52bc0da8c2
kokudolly <1226838252@qq.com>
2021-08-26 14:49:34 +0800

    feat: summary note 个人学习总结

--- summary-note/events/events.js
+++ summary-note/events/events.js
@@ -1,11 +1,11 @@
 var events = require('events')
 var eventEmitter = new events.EventEmitter()
 eventEmitter.on('say', function (name) {
-    console.log(name)
+  console.log(name)
 })
 
 eventEmitter.on('say', function (name) {
-    console.log(name)
+  console.log(name)
 })
 
 eventEmitter.emit('say', 'jiajun')
